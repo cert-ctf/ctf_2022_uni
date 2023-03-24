6dc39195e2384ffae701b2d57bb084bf2c6455f1
cert-ctf <tobias.kirschke@gematik.de>
2023-03-24 10:22:31 +0100

    init

--- README.md
+++ README.md
@@ -7,15 +7,13 @@ This is a starter kit to help you build your own map for [WorkAdventure](https:/
 To understand how to use this starter kit, follow the tutorial at [https://workadventu.re/map-building](https://workadventu.re/map-building).
 
 ## Structure
-* *public*: Static files like PDFs or audio files
-* *src*: Scripts files
-* *tilesets*: All tilesets
-* *map.tmj*: Map file
-* *map.png*: The map thumbnail displayed on the in-game map information
+* **tilesets** : All tilesets
+* **public** : Static files
+* **src** : All TypeScript/Javascript scripts
+* **map.(json/tmj)** : Map file
+* **map.png** : Image displayed on README.md and on the map infos in-game
 
-If you want to use more than one map file, just add the new map file on root or in a folder.
-
-we recommend using 500x500 images for the map thumbnails.
+If you want to use more than one map file, just add the new map file in the root folder, your tilesets in the assets folder and a new script if you need it in the src folder (it will be automaticaly optimized in production).
 
 If you are going to create custom websites to embed in the map, please reference the HTML files in the `input` option in *vite.config.js*.
 
@@ -32,12 +30,11 @@ npm install
 npm run dev
 ```
 
-## Test production map
-
-You can test the optimized map as it will be in production:
+## Test optimized map
+You can test the optimized map as you do in production:
 ```sh
 npm run build
-npm run prod
+npm run preview
 ```
 
 ## Licenses
@@ -45,11 +42,11 @@ npm run prod
 This project contains multiple licenses as follows:
 
 * [Code license](./LICENSE.code) *(all files except those for other licenses)*
-* [Map license](./LICENSE.map) *(`map.tmj` and the map visual as well)*
+* [Map license](./LICENSE.map) *(`map.json` and the map visual as well)*
 * [Assets license](./LICENSE.assets) *(the files inside the `src/assets/` folder)*
 
 ### About third party assets
 
 If you add third party assets in your map, do not forget to:
-1. Credit the author and license with the "tilesetCopyright" property present in the properties of each tilesets in the `map.tmj` file
+1. Credit the author and license with the "tilesetCopyright" property present in the properties of each tilesets in the `map.json` file
 2. Add the license text in LICENSE.assets
--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,164 @@
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-console.log('Script started successfully');
+console.log('Script started successfully!');
 
 let currentPopup: any = undefined;
+
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+	//Apotheke
+	WA.room.area.onEnter('ApothekeRoom').subscribe(() => {
+		WA.room.hideLayer("Apotheke_Roof");
+			
+		//Draw Tile in front of door to open it	
+		WA.room.setTiles([		  
+		  { x: 42, y: 23, tile: "Tile_Zone", layer: "Apotheke_OpenZone" }
+		]);
+	})  
+		
+	WA.room.area.onLeave('ApothekeRoom').subscribe(() => {
+		WA.room.showLayer("Apotheke_Roof");
+	}) 
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+	//Disco
+	WA.room.area.onEnter('DiscoRoom').subscribe(() => {
+		console.log('Hide Disco Roof');
+		WA.room.hideLayer("Disco_Roof");
+		
+		//Draw Tile in front of door to open it	
+		WA.room.setTiles([		  
+		  { x: 42, y: 23, tile: "Tile_Zone", layer: "Apotheke_OpenZone" }
+		]);
+
+	})
+	
+	WA.room.area.onLeave('DiscoRoom').subscribe(() => {
+		WA.room.showLayer("Disco_Roof");
+	}) 
+		
+	//Fischladen
+	WA.room.area.onEnter('FischladenRoom').subscribe(() => {
+		console.log('Hide Fischladen Roof');
+		WA.room.hideLayer("Fischladen_Roof");
+	})
+	
+	WA.room.area.onLeave('FischladenRoom').subscribe(() => {
+		WA.room.showLayer("Fischladen_Roof");
+	}) 
+	
+	//Popup Feder
+	WA.room.area.onEnter('feather').subscribe(() => {
+        currentPopup = WA.ui.openPopup("Popup_feather","Du hast eine Feder gefunden...",[{
+			label: "Next",
+			className: "primary",
+			callback: () => {
+				// Close the popup when the "Close" button is pressed.
+				closePopup();
+				currentPopup = WA.ui.openPopup("Popup_feather","Du hast Deine eGK und Dein Smartphone verloren...",[{
+					label: "Next",
+					className: "primary",
+					callback: () => {
+						// Close the popup when the "Close" button is pressed.
+						closePopup();
+						currentPopup = WA.ui.openPopup("Popup_feather","Vielleicht kann Dir die Rezeption weiter helfen...",[{
+							label: "Close",
+							className: "primary",
+							callback: () => {
+								// Close the popup when the "Close" button is pressed.
+								closePopup();
+								
+							}
+						}]);						
+					}
+				}]);
+			}
+		}]);
+    })
+    WA.room.area.onLeave('feather').subscribe(closePopup)
+	
+	//Popup eGK
+	WA.room.area.onEnter('erinnerung').subscribe(() => {
+        currentPopup = WA.ui.openPopup("Popup_erinnerung","Wenn ich mich nur an gestern Nacht erinnern könnte...",[]);
+    })
+    WA.room.area.onLeave('erinnerung').subscribe(closePopup) 
+
+	
+	//Popup rezeption
+	WA.room.area.onEnter('rezeption').subscribe(() => {		
+        currentPopup = WA.ui.openPopup("Popup_rezeption","Hallo! wir haben Deine eGK und Dein Smartphone leider nicht bei Dir finden können...",[{
+			label: "Next",
+			className: "primary",
+			callback: () => {
+				// Close the popup when the "Close" button is pressed.
+				closePopup();
+				currentPopup = WA.ui.openPopup("Popup_rezeption","Anscheinend hast Du auch Deine analoge Patientenakte verloren...",[{
+					label: "Next",
+					className: "primary",
+					callback: () => {
+						// Close the popup when the "Close" button is pressed.
+						closePopup();
+						currentPopup = WA.ui.openPopup("Popup_rezeption","Wenn wir Deine analoge Patientenakte hätten, könnten wir Dir sagen, was gestern Abend passiert ist...",[{
+							label: "Next",
+							className: "primary",
+							callback: () => {
+								// Close the popup when the "Close" button is pressed.
+								closePopup();
+								currentPopup = WA.ui.openPopup("Popup_rezeption","Wir empfehlen die Nutzung der ePA, damit kannst Du deine Patientenakte digital einsehen...",[{
+									label: "Next",
+									className: "primary",
+									callback: () => {
+										// Close the popup when the "Close" button is pressed.
+										closePopup();
+										currentPopup = WA.ui.openPopup("Popup_rezeption","Dafür benötigst Du aber die drei verloren Komponenten...",[{
+											label: "Close",
+											className: "primary",
+											callback: () => {
+												// Close the popup when the "Close" button is pressed.
+												closePopup();
+												
+											}
+										}]);
+										
+									}
+								}]);
+								
+							}
+						}]);						
+					}
+				}]);
+			}
+		}]);
+    })
+    WA.room.area.onLeave('rezeption').subscribe(closePopup)
+	
+	
+	
+	//Popup Apotheke
+	WA.room.area.onEnter('apotheke').subscribe(() => {
+        currentPopup = WA.ui.openPopup("Popup_Apotheke","Jeder weiss, dass man die Schätze vom ePAirat mit LostMyPass knacken kann...",[]);
+    })
+    WA.room.area.onLeave('apotheke').subscribe(closePopup)
+	
+	//Popup dj
+	WA.room.area.onEnter('dj').subscribe(() => {
+        currentPopup = WA.ui.openPopup("Popup_dj","64 79 115 105 110 116 115 66 97 114",[]);
+    })
+    WA.room.area.onLeave('dj').subscribe(closePopup)
+
+	//Popup Matrose
+	WA.room.area.onEnter('matrose').subscribe(() => {
+        currentPopup = WA.ui.openPopup("Popup_matrose","Hast Du eine Reservierungsnummer? Mir reicht auch unser E-Mail Absender...",[]);
+    })
+    WA.room.area.onLeave('matrose').subscribe(closePopup)
+
+
+		
+	 	
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -34,3 +176,6 @@ function closePopup(){
 }
 
 export {};
+
+
+
